8cd906a52d237444f543ee1300e062309ca02d7f
Henrique Canella <henrique.mandelli@gmail.com>
2020-02-28 23:41:22 -0300

    Convertendo estrutura para React Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,14 +15,16 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: "module"
   },
-  plugins: ["react", "prettier"],
+  plugins: ["react", "prettier", 'react-hooks'],
   rules: {
     "prettier/prettier": "error",
     "react/jsx-filename-extension": ["warn", { extensions: [".jsx", "js"] }],
     "import/prefer-default-export": "off",
     "react/state-in-constructor": "off",
     "no-param-reassign": "off",
-    "no-console": ["error", {allow: ["tron"]}]
+    "no-console": ["error", {allow: ["tron"]}],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
   },
   parser: "babel-eslint"
 };
